5114297c3e6e718f7b0c79c47629b522897b4665
Suraj Gharat <mr.surajgharat2@gmail.com>
2019-10-06 16:07:26 +0530

    Added comments

--- src/main/scala/one/Challenge1.scala
+++ src/main/scala/one/Challenge1.scala
@@ -1,7 +1,10 @@
 package one
 
 object Challenge1 {
+
+  // Identity function
   def myIdentity[T](arg: T): T = arg
 
+  // Composition from A->B and B->C to A->C
   def compose[A, B, C](f1: A => B, f2: B => C): A => C = x => f2(f1(x))
 }
